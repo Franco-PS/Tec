8ae424916d2755fc24b7d711bfa29bda9c3ffaca
Franco <perez_salazarf@hotmail.com>
2021-08-25 16:24:17 -0500

    arreglos de nav y mejorar js

--- asset/js/scrip.js
+++ asset/js/scrip.js
@@ -1,10 +1,25 @@
-let button= document.getElementById('menu__icon');
-button.addEventListener('click',()=>{
-    let barra= document.getElementById('menu__nav')
-    barra.classList.toggle('menu__nav--show')
-})
-let button2= document.getElementById('nav__a');
-button2.addEventListener('click',()=>{
-    let barra2= document.getElementById('nav__ulSecont')
-    barra2.classList.toggle('nav__ulSecont--show')
+// let button= document.getElementById('menu__icon');
+// button.addEventListener('click',()=>{
+//     let barra= document.getElementById('menu__nav')
+//     barra.classList.toggle('menu__nav--show')
+// })
+// let button2= document.getElementById('nav__a');
+// button2.addEventListener('click',()=>{
+//     let barra2= document.getElementById('nav__ulSecont')
+//     barra2.classList.toggle('nav__ulSecont--show')
+// })
+
+let button= document.getElementById('menu__content');
+button.addEventListener('click',e=>{
+    let t= e.target,
+        menuBarras= document.getElementById('menu__icon'),
+        menuNav= document.getElementById('menu__nav'),
+        navLink= document.getElementById('nav__a'),
+        menuSubNav= document.getElementById('nav__ulSecont');
+    if(t== menuBarras){
+        menuNav.classList.toggle('menu__nav--show')
+    }else if(t== navLink){
+        menuSubNav.classList.toggle('nav__ulSecont--show')
+    }
+    
 })
